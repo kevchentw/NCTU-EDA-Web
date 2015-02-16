f2a22e41f5d496b4903db9c7f4a9da6131d98a17
allenwhale <allenwhale@yahoo.com.tw>
2015-02-17 00:21:27 +0800

    separate GET and POST

--- EDA_Web/views.py
+++ EDA_Web/views.py
@@ -1,39 +1,66 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, authenticate, login
-
+from django.shortcuts import HttpResponse as response
 
 def home(request):
-    return render(request, "home.html")
+    if request.method == 'GET':
+        return render(request, "home.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def news(request):
-    return render(request, "news.html")
+    if request.method == 'GET':
+        return render(request, "news.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def feeds(request):
-    return render(request, "feeds.html")
+    if request.method == 'GET':
+        return render(request, "feeds.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def members(request):
-    return render(request, "members.html")
+    if request.method == 'GET':
+        return render(request, "members.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def about(request):
-    return render(request, "about.html")
+    if request.method == 'GET':
+        return render(request, "about.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def downloads(request):
-    return render(request, "downloads.html")
+    if request.method == 'GET':
+        return render(request, "downloads.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def achievement(request):
-    return render(request, "achievement.html")
+    if request.method == 'GET':
+        return render(request, "achievement.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def activity(request):
-    return render(request, "activity.html")
+    if request.method == 'GET':
+        return render(request, "activity.html")
+    elif request.method == 'POST':
+        return response('')
 
 
 def contact(request):
-    return render(request, "contact.html")
+    if request.method == 'GET':
+        return render(request, "contact.html")
+    elif request.method == 'POST':
+        return response('')
 
