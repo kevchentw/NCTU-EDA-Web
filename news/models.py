eea3eaab53c7929ff1634d74f0f404d2ddc54970
Kevin Chen <me@kevchentw.com>
2015-02-19 22:27:31 +0800

    add boolean field "modified"

--- news/models.py
+++ news/models.py
@@ -2,22 +2,25 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
-# Create your models here.
+
 class NewsModel(models.Model):
-    nid = models.AutoField(primary_key = True)
-    top = models.BooleanField(default = False)
-    created_time = models.DateTimeField(editable = False)
+    nid = models.AutoField(primary_key=True)
+    top = models.BooleanField(default=False)
+    created_time = models.DateTimeField(editable=False)
     modified_time = models.DateTimeField()
     title = models.TextField()
     content = models.TextField()
     author = models.TextField()
     classification = models.TextField()
+    modified = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.nid)
 
-    def save(self,*args,**kwargs):
+    def save(self, *args, **kwargs):
         if not self.nid:
             self.created_time = datetime.datetime.now().replace(tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
+        else:
+            self.modified = True
         self.modified_time = datetime.datetime.now().replace(tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
-        return super(NewsModel,self).save(*args,**kwargs)
+        return super(NewsModel, self).save(*args, **kwargs)
